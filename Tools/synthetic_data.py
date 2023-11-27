1517231dbb182c3f69c2f94d8482ca590a67992b
MikraMikra <krav.michael.ba@gmail.com>
2023-11-27 08:42:58 +0100

    added try catch method

--- Tools/synthetic_data.py
+++ Tools/synthetic_data.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import shutil
 
+
 def generate_random_rotation_matrix():
     # Generiere zufällige Winkel in Radiant für die Rotation um jede Achse
     angle_x = np.random.uniform(0, 2 * np.pi)
@@ -37,6 +38,7 @@ def generate_random_rotation_matrix():
 
     return final_rotation_matrix
 
+
 def create_label_file(label_path, class_id, x_center, y_center, width, height, bg_width, bg_height):
     # Konvertiere die x- und y-Koordinaten in Bezug auf das Koordinatensystem mit Ursprung in der oberen linken Ecke
     x_min = x_center - width / 2
@@ -60,93 +62,107 @@ stl_files = [
     r'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/Planetengetriebe_meshes/planet_c.stl'
 ]
 
-images_folder = r'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir'
+images_folder = r'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/Office_building'
 temp_folder = r'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp'
 
 for i, image_file in enumerate(os.listdir(images_folder)):
-    if image_file.endswith(".png") or image_file.endswith(".jpg"):
-        image_path = os.path.join(images_folder, image_file)
+    try:
+        if image_file.endswith(".png") or image_file.endswith(".jpg"):
+            image_path = os.path.join(images_folder, image_file)
 
-        for j, your_stl_file in enumerate(stl_files):
-            # Lade STL-Datei und erstelle Mesh
-            mesh = pv.read(your_stl_file)
+            for j, your_stl_file in enumerate(stl_files):
+                try:
+                    # Lade STL-Datei und erstelle Mesh
+                    mesh = pv.read(your_stl_file)
 
-            # Generiere zufällige Rotation
-            rotation_matrix = generate_random_rotation_matrix()
+                    # Generiere zufällige Rotation
+                    rotation_matrix = generate_random_rotation_matrix()
 
-            # Anwenden von Rotation auf das 3D-Objekt
-            mesh.transform(rotation_matrix)
+                    # Anwenden von Rotation auf das 3D-Objekt
+                    mesh.transform(rotation_matrix)
 
-            if j == 2:
-                # Visualisiere das 3D-Objekt
-                plotter = pv.Plotter(off_screen=True)
-                plotter.add_mesh(mesh, color='black', show_edges=False)
-            else:
-                # Visualisiere das 3D-Objekt
-                plotter = pv.Plotter(off_screen=True)
-                plotter.add_mesh(mesh, color='white', show_edges=False)
+                    if j == 2:
+                        # Visualisiere das 3D-Objekt
+                        plotter = pv.Plotter(off_screen=True)
+                        plotter.add_mesh(mesh, color='black', show_edges=False)
+                    else:
+                        # Visualisiere das 3D-Objekt
+                        plotter = pv.Plotter(off_screen=True)
+                        plotter.add_mesh(mesh, color='white', show_edges=False)
 
-            # Screenshot speichern
-            screenshot_path = f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp/{i}_{j}.png'
-            plotter.screenshot(screenshot_path, transparent_background=True)
-            plotter.close()
+                    # Screenshot speichern
+                    screenshot_path = f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp/{i}_{j}.png'
+                    plotter.screenshot(screenshot_path, transparent_background=True)
+                    plotter.close()
+                except Exception as e:
+                    print(f"Fehler beim Verarbeiten der STL-Datei {your_stl_file}: {e}")
+                    continue
 
-        # Laden Sie das Hintergrundbild
-        background_image = Image.open(image_path)
+            # Laden Sie das Hintergrundbild
+            background_image = Image.open(image_path)
 
-        for x, screenshot_path in enumerate(os.listdir(temp_folder)):
-            # Laden Sie das gespeicherte Bild, das eingefügt werden soll
-            rotated_image_path = screenshot_path
-            rotated_image = Image.open(f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp/{rotated_image_path}')
+            for x, screenshot_path in enumerate(os.listdir(temp_folder)):
+                try:
+                    # Laden Sie das gespeicherte Bild, das eingefügt werden soll
+                    rotated_image_path = screenshot_path
+                    rotated_image = Image.open(
+                        f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp/{rotated_image_path}')
 
-            scaling_factor = np.random.uniform(0.1, 0.5)
+                    scaling_factor = np.random.uniform(0.1, 0.5)
 
-            # Größe des Screenshots ändern (verkleinern oder vergrößern)
-            new_size = (int((rotated_image.width * scaling_factor)),
-                        int((rotated_image.height * scaling_factor)))
+                    # Größe des Screenshots ändern (verkleinern oder vergrößern)
+                    new_size = (int((rotated_image.width * scaling_factor)),
+                                int((rotated_image.height * scaling_factor)))
 
-            test_new_size = (int(((rotated_image.width - 520) * scaling_factor)),
-                             int(((rotated_image.height - 240) * scaling_factor)))
+                    test_new_size = (int(((rotated_image.width - 520) * scaling_factor)),
+                                     int(((rotated_image.height - 240) * scaling_factor)))
 
-            rotated_image = rotated_image.resize(new_size)
+                    rotated_image = rotated_image.resize(new_size)
 
-            # Einfügen des veränderten Bildes in das Hintergrundbild in der Mitte
-            bg_width, bg_height = background_image.size
-            rotated_width, rotated_height = rotated_image.size
-            paste_position = (np.random.randint(0, bg_width - rotated_width),
-                              np.random.randint(0, bg_height - rotated_height))
-            background_image.paste(rotated_image, paste_position, rotated_image)
+                    # Einfügen des veränderten Bildes in das Hintergrundbild in der Mitte
+                    bg_width, bg_height = background_image.size
+                    rotated_width, rotated_height = rotated_image.size
+                    paste_position = (np.random.randint(0, bg_width - rotated_width),
+                                      np.random.randint(0, bg_height - rotated_height))
+                    background_image.paste(rotated_image, paste_position, rotated_image)
 
-            # Speichern Sie das endgültige Bild
-            final_image_path = f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/images/{i}.png'
-            background_image.save(final_image_path)
+                    # Speichern Sie das endgültige Bild
+                    final_image_path = f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/images/{i}.png'
+                    background_image.save(final_image_path)
 
-            # Kopiere die PNG-Datei als JPG-Datei
-            shutil.copy(final_image_path,
-                        f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/images/{i}.jpg')
-            os.remove(final_image_path)
+                    # Kopiere die PNG-Datei als JPG-Datei
+                    shutil.copy(final_image_path,
+                                f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/images/{i}.jpg')
+                    os.remove(final_image_path)
 
-            output_dir = "/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/labels"
-            label_path = os.path.join(output_dir, f"{i}.txt")
-            file_name = os.path.basename(rotated_image_path)  # Get the file name from the path
-            # print(f"File Name: {file_name}, Type: {type(file_name)}")  # Add this line
-            if file_name[-5] == "0":
-                class_id = 0
-            elif file_name[-5] == "1":
-                class_id = 1
-            elif file_name[-5] == "2":
-                class_id = 2
-            x_center, y_center = paste_position[0] + new_size[0] / 2, paste_position[1] + new_size[1] / 2
-            # print(paste_position[0])
-            width, height = test_new_size
+                    output_dir = "/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/labels"
+                    label_path = os.path.join(output_dir, f"{i}.txt")
+                    file_name = os.path.basename(rotated_image_path)  # Get the file name from the path
+                    # print(f"File Name: {file_name}, Type: {type(file_name)}")  # Add this line
+                    if file_name[-5] == "0":
+                        class_id = 0
+                    elif file_name[-5] == "1":
+                        class_id = 1
+                    elif file_name[-5] == "2":
+                        class_id = 2
+                    x_center, y_center = paste_position[0] + new_size[0] / 2, paste_position[1] + new_size[1] / 2
+                    # print(paste_position[0])
+                    width, height = test_new_size
 
-            create_label_file(label_path, class_id, x_center, y_center, width, height, bg_width, bg_height)
+                    create_label_file(label_path, class_id, x_center, y_center, width, height, bg_width, bg_height)
 
-            # shutil.copy(f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp/{rotated_image_path}',
-            #             '/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp_2')
+                    # shutil.copy(f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp/{rotated_image_path}',
+                    #             '/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp_2')
 
-            # Löschen des Screenshots des 3D-Modells
-            os.remove(f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp/{rotated_image_path}')
+                    # Löschen des Screenshots des 3D-Modells
+                    os.remove(
+                        f'/Users/michaelkravt/PycharmProjects/BA_Repo/Tools/MainDir/TestDir/temp/{rotated_image_path}')
+                except Exception as e:
+                    print(f"Fehler beim Verarbeiten des Bildes {image_file}: {e}")
+                    continue
 
+            print(f"Verarbeite Bild {i + 1}: {image_path}")
 
-        print(f"Verarbeite Bild {i + 1}: {image_path}")
+    except Exception as e:
+        print(f"Fehler beim Verarbeiten des Bildes {image_file}: {e}")
+        continue
